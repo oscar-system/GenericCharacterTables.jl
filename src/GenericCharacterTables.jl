c550ac4c49c98b9c1e7806d63f2a7334d2cb04b0
Martin Wagner <martin.wagner.dev@gmail.com>
2024-09-17 15:58:31 +0200

    remove some unneeded code

--- src/GenericCharacterTables.jl
+++ src/GenericCharacterTables.jl
@@ -19,7 +19,6 @@ module GenericCharacterTables
 using Oscar
 import Oscar.AbstractAlgebra.Generic
 
-const FracPoly{T} = Generic.UnivPoly{Generic.FracFieldElem{T}} where T
 const NfPoly = Union{PolyRingElem{QQFieldElem}, PolyRingElem{AbsSimpleNumFieldElem}}
 
 include("GenericCyclotomics.jl")
