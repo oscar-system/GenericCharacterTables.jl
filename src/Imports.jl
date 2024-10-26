b3ade6846103efe473ed2a5f34db56c668953f97
Martin Wagner <martin.wagner.dev@gmail.com>
2024-10-26 23:04:38 +0200

    Don't import from `Oscar.AbstractAlgebra`

--- src/Imports.jl
+++ src/Imports.jl
@@ -34,38 +34,36 @@ import Oscar.RandomExtensions:
 	RandomExtensions,
 	Make2
 
-import Oscar.AbstractAlgebra:
+import Oscar:
 	add!,
 	base_ring,
 	base_ring_type,
 	canonical_unit,
 	characteristic,
+	class_multiplication_coefficient,
+	Dedent,
+	degree,
 	divexact,
 	elem_type,
 	evaluate,
 	expressify,
 	get_cached!,
+	Indent,
 	is_domain_type,
 	isequal,
 	is_exact_type,
 	is_unit,
 	mul!,
-	normal_form,
-	parent,
-	parent_type,
-	Ring,
-	RingElem,
-	zero!
-
-import Oscar:
-	class_multiplication_coefficient,
-	Dedent,
-	degree,
-	Indent,
 	norm,
+	normal_form,
 	number_of_conjugacy_classes,
 	order,
+	parent,
+	parent_type,
 	pretty,
+	Ring,
+	RingElem,
 	scalar_product,
-	tensor_product
+	tensor_product,
+	zero!
 
