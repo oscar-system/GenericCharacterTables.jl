c6223dc494679ecfa644dd3d0a39805bd07939b7
Max Horn <max@quendi.de>
2025-01-16 14:11:53 +0100

    Optimize computation of least common denominator (#241)
    * Optimize computation of least common denominator
    
    * Restore Oscar 1.1 compatibility
    
    ---------
    
    Co-authored-by: Martin Wagner <martin.wagner.dev@gmail.com>

--- src/GenericCyclotomics.jl
+++ src/GenericCyclotomics.jl
@@ -1,3 +1,15 @@
+if pkgversion(Oscar) < v"1.2.0"
+  function Base.denominator(a::AbstractAlgebra.Generic.MPoly{QQFieldElem})
+    # TODO why is lcm of empty list not defined?
+    denominators = [denominator(c) for c in coefficients(a)]
+    if isempty(denominators)
+      d = ZZ(1)
+    else
+      d = lcm(denominators)
+    end
+  end
+end
+
 @doc raw"""
     kempner_with_data(m::Int64)
 
@@ -374,6 +386,7 @@ function (R::GenericCycloRing)(f::Dict{UPolyFrac,UPoly}; simplify::Bool=true)  #
 
   # reduce numerators modulo denominators
   L = NTuple{4,UPoly}[]
+  d = 1
   for (g, c) in f
     if !iszero(c)
       if R.congruence === nothing
@@ -386,6 +399,9 @@ function (R::GenericCycloRing)(f::Dict{UPolyFrac,UPoly}; simplify::Bool=true)  #
       end
       a, r = divrem(numerator(gp), denominator(gp))
       push!(L, (c, denominator(gp), r, a))
+
+      # find the common denominator of the exponents of all 'a'
+      d = lcm(d, Int(denominator(a.p)))
     end
   end
 
@@ -393,22 +409,6 @@ function (R::GenericCycloRing)(f::Dict{UPolyFrac,UPoly}; simplify::Bool=true)  #
   if isempty(L)
     return R()
   end
-
-  # find the common denominator of the exponents
-  # TODO where did the denominator method for polynomials go?
-  # TODO why is lcm of empty list not defined?
-  denominators = [denominator(c) for (_, _, _, a) in L for c in coefficients(a)]
-  if isempty(denominators)
-    d = ZZ(1)
-  else
-    d = lcm(denominators)
-  end
-
-  # check if d fits into an Int64 as it needs to be an Int64 later
-  if !fits(Int64, d)
-    throw(OverflowError("Denominators are too big!"))
-  end
-  d = Int64(d)
 
   fp = Dict{UPolyFrac,UPoly}()
   for (c, g_2, r, a) in L
