a5b4c233a607c39535dd15e20eb34d0f64593a99
Martin Wagner <martin.wagner.dev@gmail.com>
2025-05-21 15:15:53 +0200

    Show Green functions in docs

--- docs/generate_overview.jl
+++ docs/generate_overview.jl
@@ -3,35 +3,37 @@ using GenericCharacterTables
 
 println("@__DIR__")
 
-tabletype = "Tables"
 path = joinpath(@__DIR__, "..", "data")
-table_types = readdir("$path/$tabletype")
 
-open(joinpath(@__DIR__, "src", "tables_list.md"), "w") do out
-  println(out, """
-               ```@meta
-               CurrentModule = GenericCharacterTables
-               DocTestSetup = :(using GenericCharacterTables, Oscar)
-               ```
-               
-               # List of tables
-               """)
-  for tt in table_types
-    println("# Table type $tt")
-    # TODO: provide a map from table types to "nice" versisons, e.g. `2A2` -> `{}^2A_2`
+for (tabletype, header, filename) in (("Tables", "tables", "tables_list.md"), ("Greenfunctions", "Green functions", "greenfunctions_list.md"))
+  open(joinpath(@__DIR__, "src", filename), "w") do out
+    println("# List of $header")
     println(out, """
-                 ## Table type $tt
+                 ```@meta
+                 CurrentModule = GenericCharacterTables
+                 DocTestSetup = :(using GenericCharacterTables, Oscar)
+                 ```
+                 
+                 # List of $header
                  """)
-
-    tables = [replace(x, ".jl" => "") for x in readdir("$path/$tabletype/$tt")]
-    for name in tables
-      println("- Table $tt/$name")
+    table_types = readdir("$path/$tabletype")
+    for tt in table_types
+      println("# Table type $tt")
+      # TODO: provide a map from table types to "nice" versisons, e.g. `2A2` -> `{}^2A_2`
       println(out, """
-                   ### Table `$name`
+                   ## Table type $tt
                    """)
-      g = generic_character_table(name)
-      println(out, g.information)
-      println(out)
+
+      tables = [replace(x, ".jl" => "") for x in readdir("$path/$tabletype/$tt")]
+      for name in tables
+        println("- Table $tt/$name")
+        println(out, """
+                     ### Table `$name`
+                     """)
+        g = generic_character_table(name)
+        println(out, g.information)
+        println(out)
+      end
     end
   end
 end
