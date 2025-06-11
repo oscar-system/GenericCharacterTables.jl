4f7e18beaf11b47d7d43948054c5eb494cd1096e
Martin Wagner <martin.wagner.dev@gmail.com>
2025-06-11 15:12:48 +0200

    Fix `q0` printing

--- data/Tables/2B2/2B2.jl
+++ data/Tables/2B2/2B2.jl
@@ -2,7 +2,7 @@ using ..GenericCharacterTables
 import ..GenericCharacterTables:
   Parameters, Parameter, CharTable, GenericCyclo, GenericCycloFrac, nesum
 using ..GenericCharacterTables.Oscar
-S, E, q0 = generic_cyclotomic_ring()
+S, E, q0 = generic_cyclotomic_ring(; variable=:q0)
 R = base_ring(S)
 a, b, c, s, k, u, _... = params(
   S,
